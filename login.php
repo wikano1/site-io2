167fe887dde5dedfae46500ac79c48bb94d4e1ce
wikano1 <95180898+wikano1@users.noreply.github.com>
2022-03-29 00:25:51 +0200

    Delete login.php

--- login.php
+++
@@ -1,7 +0,0 @@
-<?php
-   include("connexionmysql.php");
-   $conn=connexion();
-   $res = mysqli_query($conn,"SELECT * FROM utilisateur WHERE nom = 'oui' AND motdepasse = 'non'");
-   $nb = mysqli_num_rows($res);
-   echo $nb;
-?>
